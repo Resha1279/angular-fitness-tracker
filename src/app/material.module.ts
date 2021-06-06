9824e80763a32a57712e817e08512cc315e332b6
Resha1279 <resha.adhikari1279@gmail.com>
2021-06-06 22:31:49 +0545

    added few components and created form for signup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
 
   imports: [
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
   ],
 })
 
